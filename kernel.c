1c37201f7d39b174b7dcc2ddd70ddfc170226f22
Airbone25 <keshavmehra2005@gmail.com>
2026-06-03 14:40:07 +0530

    feat: add new line and line wrapping in putchar

--- kernel.c
+++ kernel.c
@@ -1,15 +1,33 @@
 char* video = (char*)0xB8000;
 
+int row = 0;
+int col = 0;
+
+void putchar(char c){
+  if(c == '\n'){
+    col = 0;
+    row++;
+    return;
+  }
+  if(col>=80){
+    col = 0;
+    row++;
+  }
+  int pos = ((row*80)+col)*2;
+  video[pos] = c;
+  video[pos+1] = 0x07;
+  col++;
+}
+
 void print(const char* s){
-  for(int i=0;s[i] != 0;i++){
-    video[i*2] = s[i];
-    video[i*2+1] = 0x07;
+  while(*s){
+    putchar(*s++);
   }
 }
 
 void kernel_main(){
-	
-	print("Hello World From MyOS");
-	print("Welcome");
+	for(int i=0;i<30;i++)
+    print("Hello\n");
+  print("This is MyOS");
 	while(1);
 }
